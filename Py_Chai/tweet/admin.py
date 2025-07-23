a8be4f6f10a9577e12fde3149b58c9b4e190fb8d
Zunair-arhamsoft <zunair.saeed@arhamsoft.com>
2025-07-23 15:32:12 +0500

    Registered the tweet model to admin

--- Py_Chai/tweet/admin.py
+++ Py_Chai/tweet/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tweet   
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'created_at', 'updated_at')
+    search_fields = ('user__username', 'text')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
